b1fe012ed973a40a0289fc480ea85dfe1af7280c
deadxraver <409853@edu.itmo.ru>
2025-04-03 14:17:08 +0300

    file open

--- src/translator/main.rs
+++ src/translator/main.rs
@@ -1,5 +1,10 @@
 pub mod srs_commands;
+use std::io::Read;
 
 fn main() {
-    srs_commands::test();
+    let path = std::env::args().nth(1).expect("Please enter filename");
+    let mut file = std::fs::File::open(path).expect("File not found");
+    let mut contents = String::new();
+    file.read_to_string(&mut contents).expect("Unexpected error while reading file contents");
+    print!("{contents}");
 }
