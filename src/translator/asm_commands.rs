607ac5bebb1347048aa09d441403e2edb93bd36b
deadxraver <409853@edu.itmo.ru>
2025-04-03 22:51:08 +0300

    remove unnecessary functions + option for enum to print its name

--- src/translator/asm_commands.rs
+++ src/translator/asm_commands.rs
@@ -3,6 +3,7 @@ const SHIFT: u8 = 8;
 const OPCODE_MASK: u64 = 0x0FF0;
 const OPERAND_MASK: u64 = 0x000F;
 
+#[derive(Debug)]
 pub enum Opcodes {
     PushImm0 = 0x00,
     PushImm1,
@@ -36,21 +37,11 @@ fn shift_left(num: u64) -> u32 {
     (num << SHIFT) as u32
 }
 
-fn shift_right(num: u64) -> u32 {
-    (num >> SHIFT) as u32
-}
 
 pub struct MachineInstruction {
     opcode: Opcodes,
     operand: u8,
 }
-
-/*
-* FIXME: cast number to enum
-pub fn from_bytes(instruction: u32) -> MachineInstruction {
-    MachineInstruction { opcode: Opcodes { shift_right(instruction as u64 & OPCODE_MASK) }, operand: (instruction as u64 & OPERAND_MASK) as u8 }
-}
-*/
 
 pub fn to_bytes(instruction: MachineInstruction) -> u32 {
     instruction.operand as u32 + shift_left(instruction.opcode as u64)
--- src/translator/main.rs
+++ src/translator/main.rs
@@ -7,6 +7,8 @@ fn main() {
     let mut file = std::fs::File::open(path).expect("File not found");
     let mut contents = String::new();
     file.read_to_string(&mut contents).expect("Unexpected error while reading file contents");
+    // TODO: remove everything below this line, it is for testing components
     print!("{contents}");
-    println!("{:#02x}", asm_commands::Opcodes::Add as u16);
+    println!("{:#04x}", asm_commands::Opcodes::Add as u16);
+    println!("{:?}", asm_commands::Opcodes::Add);
 }
