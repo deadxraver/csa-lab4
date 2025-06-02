51c0998ddb103aca59d8dce3d6a10221583418b5
Даниил <deadxraver@yandex.ru>
2025-06-02 17:01:44 +0300

    asm commands description

--- src/translator/asm_commands.rs
+++ src/translator/asm_commands.rs
@@ -5,28 +5,28 @@ const OPCODE_MASK: u64 = 0x000F;
 #[allow(dead_code)]
 #[derive(Debug)]
 pub enum Opcodes {
-    PushImm = 0x00,
-    Add,
-    Sub,
-    Mul,
-    Div,
-    Pop1,
-    Pop2,
-    Dup,
-    To1,
-    To2,
-    Jmp,
-    BIf,
-    Store,
-    Call,
-    Ret,
-    Halt,
-    And,
-    Or,
-    Xor,
-    Not,
-    Eq,
-    PushInd,
+    PushImm = 0x00, // push immediate value to stack
+    Add,            // stack.push(stack.pop() + stack.pop())
+    Sub,            // stack.push(stack.pop() - stack.pop())
+    Mul,            // stack.push(stack.pop() * stack.pop())
+    Div,            // stack.push(stack.pop() / stack.pop())
+    Pop1,           // stack.pop()
+    Pop2,           // stack2.pop()
+    Dup,            // stack.push(stack.top())
+    To1,            // stack.push(stack2.pop())
+    To2,            // stack2.push(stack.pop())
+    Jmp,            // jump to address represented as imm val
+    BIf,            // branch if top of the stack is non-zero
+    Store,          // не помню че я хотел этим сказать
+    Call,           // call function
+    Ret,            // return from function
+    Halt,           // halt the machine
+    And,            // bitwise AND
+    Or,             // bitwise OR
+    Xor,            // bitwise XOR
+    Not,            // bitwise NOT
+    Eq,             // stack.pop() == stack.pop() ? -1 : 0
+    PushInd,        // вот это и ниже тоже не помню что изначально должно было значить потом поменяю
     StoreInd,
 }
 
