a092fabaa1bee8bbfb154dc9a144d41f6f7af9a5
chimi <chimi@chimi>
2025-07-17 21:21:04 +0300

    CMake project structure + CI config update

---
+++ src/main.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[]) { // TODO: configure formatter to stick * to
+                                   // variable type instead of name
+  printf("%s\n", "test");
+  return 0;
+}
---
+++ test/test1.c
@@ -0,0 +1,5 @@
+#include <assert.h>
+int main() {
+  assert(1 + 1 == 2);
+  return 0;
+}
