374506297c436e5e9b8ad22d74d19061430f3371
deadxraver <409853@edu.itmo.ru>
2025-08-15 20:15:20 +0300

    added useful fields to ParseResult struct, added default value for it, replaced no arg error with no file error, included string header for strcmp

--- src/translator/argparser.h
+++ src/translator/argparser.h
@@ -1,9 +1,10 @@
 #include <stdio.h>
 #include <stdbool.h>
+#include <string.h>
 
 enum ErrorCode {
   NO_ERROR = 0,
-  NO_ARGS_ERROR,
+  NO_FILE_ERROR,
   UNKNOWN_ARG_ERROR,
   NO_SUCH_FILE,
 };
@@ -13,7 +14,18 @@ struct ParseResults {
   size_t pos;
   bool verbose;
   bool preprocess_only;
+  bool help_message_only;
+  char* filename;
 };
+
+const struct ParseResults DEFAULT_PARSE_RESULTS = (ParseResults) {
+  .error_code = NO_ERROR,
+  .pos = -1,
+  .verbose = false,
+  .preprocess_only = false,
+  .help_message_only = false,
+  .filename = NULL,
+}
 
 void print_message(struct ParseResults parse_results, char* argv[]);
 
