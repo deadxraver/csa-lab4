3f4592e511b30b1f4c900190370840160b2dec63
deadxraver <409853@edu.itmo.ru>
2025-08-15 21:19:35 +0300

    test for bad input

---
+++ test/translator_argparser_error.c
@@ -0,0 +1,16 @@
+#include <assert.h>
+#include "../src/translator/argparser.h"
+
+int main() {
+  int argc = 3;
+  char* argv[3] = {
+    "asm-compile",
+    "--verbose",
+    "--preprocess-only",
+  };
+  struct ParseResults expect = DEFAULT_PARSE_RESULTS;
+  expect.error_code = NO_FILE_ERROR;
+  struct ParseResults result = parse_args(argc, argv);
+  assert(expect.error_code == result.error_code);
+  return 0;
+}
