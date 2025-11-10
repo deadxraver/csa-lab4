5c36d39e9288e49170c2097ff92730f5f9fb6103
deadxraver <deadxraver@yandex.ru>
2025-11-10 10:27:03 +0300

    more verbose output on test

--- test/preprocessor_success.c
+++ test/preprocessor_success.c
@@ -7,13 +7,20 @@ char initial_code[] = "%define ONE_CONST 1\nONE_CONST";
 char preprocessed_code[] = "1";
 
 int main() {
+  int err_code = 0;
   char* result_code = preprocess_code(initial_code);
   if (result_code == NULL) {
-    fprintf(stderr, "return value is NULL, expected: %s\n", preprocessed_code);
-    return -1;
+    fprintf(stderr, "return value is NULL, expected:\n```\n%s\n```\n", preprocessed_code);
+    err_code = -1;
+    goto end;
   }
-  assert(strcmp(result_code, preprocessed_code) == 0);
+  if (strcmp(result_code, preprocessed_code) != 0) {
+    fprintf(stderr, "expected:\n```\n%s\n```\n", preprocessed_code);
+    fprintf(stderr, "got:\n```\n%s\n```\n", result_code);
+    err_code = -1;
+  }
+end:
   free(result_code);
   result_code = NULL;
-  return 0;
+  return err_code;
 }
